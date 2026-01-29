6fa527b622db25e7749bc1499b754012e6ed8b69
Ashish Rajput <ashishrajput040807@gmail.com>
2026-01-29 23:52:20 +0530

    wow

--- first.java
+++ first.java
@@ -1,37 +1,4 @@
-// public class first{
-//     public static void main (String []arg){
-//         String str = "Hello Java";
-//         int len = str.length();
-//         for (int i =0 ; i<len ;i++){
-//             char ch = str.charAt(i);
-//             if ( ch>= 'A' && ch <= 'Z'){
-//                 System.out.println((char)(ch + 32));
-//             }
-//             else if ( ch >= 'a' && ch<= 'z'){
-//                 System.out.println((char)(ch-32));
-//             }
-//             else 
-//                 System.out.println(ch);
-//         // String str1 = 12 + 7+ " * " + 5 + 3+7 ;
-//         // System.out.println(str1);
-//         }
-//     }
-// }
-           String str = "\r\r\t  abc  def\r\r\n  ";
-            str =  str.trim();
-            system.out.println(str);
-
-        str = str.replace ('a','b');
-        System.out.println(name.length());
-        System.out.println(name.charAt(1));
-        System.out.println(name);
-        System.out.println(name.toLowerCase());
-        System.out.println(name.startsWith("aNUSHAr")); 
-        System.out.println(name.contains("Adarsh"));
-    // collection of characters is STRIING 
-     
-    
-public class first{
+public class First{
     public static void main (String []arg){
         String str = "Hello Java";
         int len = str.length();
