d8b62a1cf652933cb7947924bc22be4058e3947f
Ashish Rajput <ashishrajput040807@gmail.com>
2026-02-01 22:54:15 +0530

    git

--- random_codz.java
+++ random_codz.java
@@ -1,13 +1,6 @@
-public class random_codz{
-    public static void main (String []args){
-        double price = 100.00 ;
-        double finalprice = price+18 ;
-        // System.out.println(finalprice);
-        int p = 100 ;
-        double fp = p + (int)(18.18) ;
-        System.out.println(fp);
-
-
-
+class Solution {
+    public int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+        
     }
 }
