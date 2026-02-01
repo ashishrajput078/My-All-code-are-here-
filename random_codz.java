47f7cbe4db3895f2479aa6eb23efd6613fc2a869
Ashish Rajput <ashishrajput040807@gmail.com>
2026-02-01 22:56:19 +0530

    gigi

--- random_codz.java
+++ random_codz.java
@@ -1,6 +1,15 @@
 class Solution {
-    public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
-        
+    public int findNumbers(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = String.valueOf(nums[i]).length();
+
+            if (n % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
