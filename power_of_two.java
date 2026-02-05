9aeba317bada10069e863eb49e038e1160e44e51
Ashish Rajput <ashishrajput040807@gmail.com>
2026-02-05 23:57:14 +0530

    ljlj

--- power_of_two.java
+++ power_of_two.java
@@ -1,17 +1,39 @@
-class power_of_two {
-    public boolean isPowerOfTwo(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        while (n % 2 == 0) {
-            n = n / 2;
-        }
-        return n == 1;
-    }
+HashMap<Integer, Integer> map = new HashMap<>();
+int prefixSum = 0;
+int maxLen = 0;
 
+map.put(0, -1);  // important base case
+
+for (int i = 0; i < n; i++) {
+    prefixSum += arr[i] - 1;
+
+    if (map.containsKey(prefixSum)) {
+        int len = i - map.get(prefixSum);
+        maxLen = Math.max(maxLen, len);
+    } else {
+        map.put(prefixSum, i);
+    }
+}
+public class Main {
     public static void main(String[] args) {
-        power_of_two obj = new power_of_two();
-        int n = 1;   // yahan number change karke test karo
-         System.out.println(obj.isPowerOfTwo(n));
+        int[] arr = {1, 2, 1, 1, 1, 3, 2};
+
+        int n = arr.length;
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                int len = j - i + 1;
+
+                if (sum == len) {
+                    maxLen = Math.max(maxLen, len);
+                }
+            }
+        }
+
+        System.out.println(maxLen);
     }
 }
