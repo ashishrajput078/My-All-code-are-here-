8a89b98ab0d8b47ba4f3123c136a03c7061cc098
Ashish Rajput <ashishrajput040807@gmail.com>
2026-02-07 23:15:05 +0530

    koko

--- first.java
+++ first.java
@@ -1,18 +1,11 @@
-public class First{
-    public static void main (String []arg){
-        int arr[] = new int [10] ;
-        int n =0;
-        int count =0; 
-        while (n>0){
-            arr[n%10]++ ;
-            n/=10;
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if (n% 2 ==0){
+            return n ;
         }
-        for (int i =0 ; i <10 ; i++){
-            if(arr[i] != 0){
-                count++ ;
-            }
-        }        System.out.println(count);
-
-      
+        if (n%2!=0){
+            return n*2 ;
+        }
+        return n ;
     }
-}     
+}
