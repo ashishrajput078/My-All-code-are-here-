c35f43d4e936b0a011b219847ab224fa072dcbd4
Ashish Rajput <ashishrajput040807@gmail.com>
2026-02-07 23:16:44 +0530

    lol

--- Main.java
+++ Main.java
@@ -1,21 +1,11 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        int n = 122333;
-
-        int[] freq = new int[10]; // 0 to 9 digits
-
-        while (n > 0) {
-            int digit = n % 10;
-            freq[digit]++;
-            n /= 10;
+class smallevenretrn {
+    public int smallestEvenMultiple(int n) {
+        if (n% 2 ==0){
+            return n ;
         }
-
-        for (int i = 0; i < 10; i++) {
-            if (freq[i] > 0) {
-                System.out.println(i + " -> " + freq[i]);
-            }
+        if (n%2!=0){
+            return n*2 ;
         }
+        return n ;
     }
 }
--- first.java
+++ first.java
@@ -1,11 +1,18 @@
-class Solution {
-    public int smallestEvenMultiple(int n) {
-        if (n% 2 ==0){
-            return n ;
+public class First{
+    public static void main (String []arg){
+        int arr[] = new int [10] ;
+        int n =0;
+        int count =0; 
+        while (n>0){
+            arr[n%10]++ ;
+            n/=10;
         }
-        if (n%2!=0){
-            return n*2 ;
-        }
-        return n ;
+        for (int i =0 ; i <10 ; i++){
+            if(arr[i] != 0){
+                count++ ;
+            }
+        }        System.out.println(count);
+
+      
     }
-}
+}     
